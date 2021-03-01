8e2b1956349b06802fd0e25b0b44c3522a5edbe4
red5tart <red5tart@mail.ru>
2021-03-01 22:11:16 +0300

    lesson-3

--- functions.php
+++ functions.php
@@ -1,6 +1,32 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'enqueue_unitheme_style' );
+//Добавление расширенных возможностей
+if ( ! function_exists( 'uni_theme_setup' ) ) :
+
+  function uni_theme_setup() {
+    // Добавление тэга title 
+    add_theme_support( 'title-tag' );
+
+    // Добавление пользовательского логотипа
+    add_theme_support( 'custom-logo', [
+      'width'       => 163,
+      'flex-height' => true,
+      'header-text' => 'Uni Theme',
+      'unlink-homepage-logo' => false, // WP 5.5
+    ] );
+
+    // Регистрация меню
+    register_nav_menus( [
+      'header_menu' => 'Меню в шапке',
+      'footer_menu' => 'Меню в подвале'
+    ] );
+  }
+endif;
+add_action( 'after_setup_theme', 'uni_theme_setup' );
+
+//Подключение стилей и скриптов
+
 function enqueue_unitheme_style() {
   wp_enqueue_style( 'style', get_stylesheet_uri() );
-  wp_enqueue_style( 'uni-theme', get_template_directory_uri(  ) . '/assets/css/uni-theme.css', 'style', null, null );
+  wp_enqueue_style( 'uni-theme', get_template_directory_uri(  ) . '/assets/css/uni-theme.css', 'style');
 }
+add_action( 'wp_enqueue_scripts', 'enqueue_unitheme_style' );
--- header.php
+++ header.php
@@ -1,8 +1,34 @@
-<html lang="en">
+<!DOCTYPE html> <!-- начало 3 урока 18:44, скорость 1.4 -->
+<html <?php language_attributes(); ?>>
 <head>
-  <meta charset="UTF-8">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Universal</title>
   <?php wp_head(  ); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<header class="header">
+  <div class="container">
+    <div class="header-wrapper">
+      <?php 
+        if( has_custom_logo() ){
+          the_custom_logo();
+        } else {
+          echo 'Uni Theme';
+        }
+      ?>
+      <?php 
+        wp_nav_menu( [
+          'theme_location'  => 'header_menu',
+          'container'       => 'nav', 
+          'container_class' => 'header-nav', 
+          'menu_class'      => 'header-menu', 
+          'echo'            => true,
+        ] );
+      ?>
+      <?php get_search_form(); ?>
+    </div>
+    <!-- /.header-wrapper -->
+  </div>
+</header>
