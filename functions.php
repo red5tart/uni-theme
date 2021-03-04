c93b5ab47e73ebd0361352648759eeb90cd97f33
red5tart <red5tart@mail.ru>
2021-03-05 00:48:41 +0300

    less thumbs and fix frontpage  cover

--- functions.php
+++ functions.php
@@ -34,3 +34,15 @@ function enqueue_unitheme_style() {
   wp_enqueue_style( 'Roboto-Slab', 'https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap');
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_unitheme_style' );
+
+## отключаем создание миниатюр файлов для указанных размеров
+add_filter( 'intermediate_image_sizes', 'delete_intermediate_image_sizes' );
+function delete_intermediate_image_sizes( $sizes ){
+	// размеры которые нужно удалить
+	return array_diff( $sizes, [
+		'medium_large',
+		'large',
+		'1536x1536',
+		'2048x2048',
+	] );
+}
