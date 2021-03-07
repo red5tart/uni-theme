0fa1a303db332f795613b25852ffd38d71793c27
red5tart <red5tart@mail.ru>
2021-03-07 12:53:15 +0300

    L.5: added block of 4 articles

--- front-page.php
+++ front-page.php
@@ -8,20 +8,25 @@
         global $post;
 
         $myposts = get_posts([ 
-          'numberposts' => 1,
-        ]);
-        
-        //проверяем, есть ли посты
-        if( $myposts ){
-          //если есть, запускаем цикл
-          foreach( $myposts as $post ){
-            setup_postdata( $post );
-            ?>
-            <!-- Выводим записи -->
-        <img src="<?php the_post_thumbnail_url() ?>" alt="" class="post-thumb">
+          'numberposts' => 1, 
+        ]); 
+        //проверяем, есть ли посты 
+        if( $myposts ){ 
+        //если есть, запускаем цикл 
+        foreach( $myposts as $post ){ setup_postdata( $post ); ?>
+        <!-- Выводим записи -->
+        <img
+          src="<?php the_post_thumbnail_url() ?>"
+          alt=""
+          class="post-thumb"
+        />
         <?php $author_id = get_the_author_meta('ID') ?>
         <a href="<?php echo get_author_posts_url($author_id) ?>" class="author">
-          <img src="<?php echo get_avatar_url($author_id) ?>" alt="" class="avatar">
+          <img
+            src="<?php echo get_avatar_url($author_id) ?>"
+            alt=""
+            class="avatar"
+          />
           <div class="author-bio">
             <span class="author-name"><?php the_author(); ?></span>
             <span class="author-rank">Должность</span>
@@ -29,14 +34,16 @@
         </a>
         <div class="post-short">
           <?php the_category(); ?>
-          <h2 class="post-title"><?php the_title(); ?></h2>
+          <h2 class="post-title"><?php echo mb_strimwidth(get_the_title(), 0, 60, '...'); ?></h2>
           <a href="<?php echo get_permalink(); ?>" class="more">Читать далее</a>
         </div>
         <?php 
         }
       } else {
         // Постов не найдено
-        ?> <p>Постов нет</p> <?php
+        ?>
+        <p>Постов нет</p>
+        <?php
       }
 
       wp_reset_postdata(); // Сбрасываем $post
@@ -51,29 +58,28 @@
           global $post;
 
           $myposts = get_posts([
-            'offset' => 1,
-            'numberposts' => 5,
-            'orderby' => 'date',
-          
-          ]);
-          
-          //проверяем, есть ли посты
-          if( $myposts ){
-            //если есть, запускаем цикл
-            foreach( $myposts as $post ){
-              setup_postdata( $post );
-              ?>
-              <!-- Выводим записи -->
-              <li class="post">
-                <?php the_category(); ?>
-                <a class="post-permalink" href="<?php echo get_the_permalink(); ?>">
-                  <h4 class="post-title"><?php the_title(); ?></h4>
-                </a>
-              </li>
-              <?php 
+            'offset' => 1, 
+            'numberposts' => 5, 
+            'orderby' => 'date', 
+            'category_name' => 'javascript, css, html, web-design', 
+          ]); 
+          //проверяем, есть ли посты 
+          if( $myposts ){ 
+          //если есть, запускаем цикл 
+          foreach( $myposts as $post ){ setup_postdata( $post ); ?>
+          <!-- Выводим записи -->
+          <li class="post">
+            <?php the_category(); ?>
+            <a class="post-permalink" href="<?php echo get_the_permalink(); ?>">
+              <h4 class="post-title"><?php echo mb_strimwidth(get_the_title(), 0, 60, '...'); ?></h4>
+            </a>
+          </li>
+          <?php 
               }
             } else {
-              ?> <p>Постов нет</p> <?php
+              ?>
+          <p>Постов нет</p>
+          <?php
             }
           wp_reset_postdata(); // Сбрасываем $post
           ?>
@@ -85,3 +91,39 @@
   </div>
   <!-- /.container -->
 </main>
+
+<!-- начало раздела серой плашки под блоком hero -->
+<div class="container">
+  <ul class="article-list">
+    <?php 
+      //объявляем глобальную переменную
+      global $post;
+  
+      $myposts = get_posts([
+        'numberposts' => 4, 
+        'category_name' => 'articles', 
+      ]); 
+      //проверяем, есть ли посты
+      if( $myposts ){ 
+        //если есть, запускаем цикл 
+        foreach( $myposts as $post ){
+        setup_postdata( $post ); 
+        ?>
+      <!-- Выводим записи -->
+      <li class="article-item">
+        <a class="article-permalink" href="<?php echo get_the_permalink(); ?>">
+          <h4 class="article-title"><?php echo wp_trim_words(get_the_title(), 6, '...'); ?></h4>
+        </a>
+        <img width="65" height="65" src="<?php echo get_the_post_thumbnail_url( null, 'thumbnail')?>" alt="">
+      </li>
+      <?php 
+        }
+      } else {
+        ?><p>Постов нет</p><?php
+      }
+    wp_reset_postdata(); // Сбрасываем $post
+    ?>
+  </ul>
+</div>
+<!-- /.container -->
+<!--конец раздела серой плашки под блоком hero -->
