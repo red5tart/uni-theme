5ec60aab57f5a85568eccd705877db5a2ca2774d
red5tart <red5tart@mail.ru>
2021-03-08 14:28:54 +0300

    7: sidebar widget, rev0 w/style errors

--- front-page.php
+++ front-page.php
@@ -126,145 +126,151 @@
   </ul>
   <!-- /.article-list - конец раздела серой плашки под блоком hero -->
 
-  <ul class="article-grid">
-    <?php		
-      global $post;
-      //формируем запрос в базу данных
-      $query = new WP_Query( [
-        //получаем семь постов
-        'posts_per_page' => 7,
-      ] );
-      //проверяем,есть  ли посты
-      if ( $query->have_posts() ) {
-        //создаем переменную-счетчик постов
-        $cnt = 0;
-        //пока посты есть, выводим их
-        while ( $query->have_posts() ) {
-          $query->the_post();
-          //и увеличиваем счетчик постов
-          $cnt++;
-          switch ($cnt) {
-            //выводим первый пост
-            case '1':
-              ?>
-                <li class="article-grid-item article-grid-item-1">
-                  <a href="<?php echo the_permalink()?>" class="article-grid-permalink">
-                    <span class="category-name"><?php $category = get_the_category(); echo $category[0]->name;?></span>
-                    <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 60, '...');
-                    ?></h4>
-                    <p class="article-grid-excerpt">
-                      <?php echo mb_strimwidth(get_the_excerpt(), 0, 120, '...'); ?>
-                    </p>
-                    <div class="article-grid-info">
-                      <div class="author">
-                        <?php $author_id = get_the_author_meta('ID') ?>
-                        <img src="<?php echo get_avatar_url($author_id) ?>" alt="" class="author-avatar">
-                        <span class="author-name"><strong><?php the_author() ?>: </strong> <?php the_author_meta('description') ?></span>
-                        <div class="comments">
-                          <img src="<?php echo get_template_directory_uri(  ) . '/assets/images/comment.svg'?>" alt="icon: comment" class="comments-icon">
-                          <span class="comments-counter">
-                            <?php comments_number('0', '1', '%') ?>
-                          </span>
-                        </div>
-                          <!-- /.comments -->
-                      </div>
-                      <!-- /.author -->
-                    </div>
-                    <!-- /.article-grid-info -->
-                  </a>
-                </li>
-              <?php
-              break;
-
-            //выводим второй пост
-            case '2': 
-              ?>
-                <li class="article-grid-item article-grid-item-2">
-                  <img src="<?php echo get_the_post_thumbnail_url()?>" alt="" class="article-grid-thumb">
-                  <a href="<?php echo the_permalink()?>" class="article-grid-permalink">
-                    <span class="tag">
-                      <?php 
-                        $posttags = get_the_tags();
-                        if ( $posttags ) {
-                          echo $posttags[0]->name . ' ';
-                        } ?>
-                    </span>
-                    <span class="category-name"><?php $category = get_the_category(); echo $category[0]->name;?></span>
-                    <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...');
-                    ?></h4>
-                    <div class="article-grid-info">
-                      <div class="author">
-                        <?php $author_id = get_the_author_meta('ID') ?>
-                        <img src="<?php echo get_avatar_url($author_id) ?>" alt="" class="author-avatar">
-                        <div class="author-info">
-                          <span class="author-name"><strong><?php the_author() ?></strong></span>
-                          <span class="date"><?php the_time( 'j F' )?></span>
+  <div class="main-grid">
+    <ul class="article-grid">
+      <?php		
+        global $post;
+        //формируем запрос в базу данных
+        $query = new WP_Query( [
+          //получаем семь постов
+          'posts_per_page' => 7,
+        ] );
+        //проверяем,есть  ли посты
+        if ( $query->have_posts() ) {
+          //создаем переменную-счетчик постов
+          $cnt = 0;
+          //пока посты есть, выводим их
+          while ( $query->have_posts() ) {
+            $query->the_post();
+            //и увеличиваем счетчик постов
+            $cnt++;
+            switch ($cnt) {
+              //выводим первый пост
+              case '1':
+                ?>
+                  <li class="article-grid-item article-grid-item-1">
+                    <a href="<?php echo the_permalink()?>" class="article-grid-permalink">
+                      <span class="category-name"><?php $category = get_the_category(); echo $category[0]->name;?></span>
+                      <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 60, '...');
+                      ?></h4>
+                      <p class="article-grid-excerpt">
+                        <?php echo mb_strimwidth(get_the_excerpt(), 0, 120, '...'); ?>
+                      </p>
+                      <div class="article-grid-info">
+                        <div class="author">
+                          <?php $author_id = get_the_author_meta('ID') ?>
+                          <img src="<?php echo get_avatar_url($author_id) ?>" alt="" class="author-avatar">
+                          <span class="author-name"><strong><?php the_author() ?>: </strong> <?php the_author_meta('description') ?></span>
                           <div class="comments">
-                            <img src="<?php echo get_template_directory_uri(  ) . '/assets/images/comment2.svg'?>" alt="icon: comment" class="comments-icon">
+                            <img src="<?php echo get_template_directory_uri(  ) . '/assets/images/comment.svg'?>" alt="icon: comment" class="comments-icon">
                             <span class="comments-counter">
                               <?php comments_number('0', '1', '%') ?>
                             </span>
                           </div>
                             <!-- /.comments -->
-                          <div class="likes">
-                            <img src="<?php echo get_template_directory_uri(  ) . '/assets/images/heart.svg'?>" alt="icon: like" class="comments-icon">
-                            <span class="comments-counter">
-                              <?php comments_number('0', '1', '%') ?>
-                            </span>
-                          </div>
-                          <!-- /.likes -->
                         </div>
-                        <!-- /.author-info -->
+                        <!-- /.author -->
                       </div>
-                      <!-- /.author -->
-                    </div>
-                    <!-- /.article-grid-info -->
-                  </a>
-                </li>
-              <?php 
-              break;  
+                      <!-- /.article-grid-info -->
+                    </a>
+                  </li>
+                <?php
+                break;
 
-            //выводим третий пост
-            case '3': 
-              ?>
-                <li class="article-grid-item article-grid-item-3">
-                  <a href="<?php echo the_permalink()?>" class="article-grid-permalink">
-                    <img class="article-thumb" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
-                    <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...');
+              //выводим второй пост
+              case '2': 
+                ?>
+                  <li class="article-grid-item article-grid-item-2">
+                    <img src="<?php echo get_the_post_thumbnail_url()?>" alt="" class="article-grid-thumb">
+                    <a href="<?php echo the_permalink()?>" class="article-grid-permalink">
+                      <span class="tag">
+                        <?php 
+                          $posttags = get_the_tags();
+                          if ( $posttags ) {
+                            echo $posttags[0]->name . ' ';
+                          } ?>
+                      </span>
+                      <span class="category-name"><?php $category = get_the_category(); echo $category[0]->name;?></span>
+                      <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...');
                       ?></h4>
-                  </a>
-                </li>
-              <?php 
-              break;
-            
-            //выводим остальные посты
-            default:
-              ?>
-                <li class="article-grid-item article-grid-item-default">
-                  <a href="<?php echo the_permalink()?>" class="article-grid-permalink">
-                    <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 40, '...');?></h4>
-                    <p class="article-grid-excerpt">
-                      <?php echo mb_strimwidth(get_the_excerpt(), 0, 70, '...');
-                    ?>
-                    </p>
-                    <span class="article-date"><?php the_time( 'j F Y' )?></span>
-                  </a>
-                </li>
-              <?php 
-              break;
-          }
-          ?>
-          <!-- Вывода постов, функции цикла: the_title() и т.д. -->
-          <?php 
-        }
-      } else {
-    // Постов не найдено
-    }
+                      <div class="article-grid-info">
+                        <div class="author">
+                          <?php $author_id = get_the_author_meta('ID') ?>
+                          <img src="<?php echo get_avatar_url($author_id) ?>" alt="" class="author-avatar">
+                          <div class="author-info">
+                            <span class="author-name"><strong><?php the_author() ?></strong></span>
+                            <span class="date"><?php the_time( 'j F' )?></span>
+                            <div class="comments">
+                              <img src="<?php echo get_template_directory_uri(  ) . '/assets/images/comment2.svg'?>" alt="icon: comment" class="comments-icon">
+                              <span class="comments-counter">
+                                <?php comments_number('0', '1', '%') ?>
+                              </span>
+                            </div>
+                              <!-- /.comments -->
+                            <div class="likes">
+                              <img src="<?php echo get_template_directory_uri(  ) . '/assets/images/heart.svg'?>" alt="icon: like" class="comments-icon">
+                              <span class="comments-counter">
+                                <?php comments_number('0', '1', '%') ?>
+                              </span>
+                            </div>
+                            <!-- /.likes -->
+                          </div>
+                          <!-- /.author-info -->
+                        </div>
+                        <!-- /.author -->
+                      </div>
+                      <!-- /.article-grid-info -->
+                    </a>
+                  </li>
+                <?php 
+                break;  
 
-    wp_reset_postdata(); // Сбрасываем $post
-    ?>
-  </ul>
-  <!-- /.article-grid -->
+              //выводим третий пост
+              case '3': 
+                ?>
+                  <li class="article-grid-item article-grid-item-3">
+                    <a href="<?php echo the_permalink()?>" class="article-grid-permalink">
+                      <img class="article-thumb" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+                      <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...');
+                        ?></h4>
+                    </a>
+                  </li>
+                <?php 
+                break;
+              
+              //выводим остальные посты
+              default:
+                ?>
+                  <li class="article-grid-item article-grid-item-default">
+                    <a href="<?php echo the_permalink()?>" class="article-grid-permalink">
+                      <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 40, '...');?></h4>
+                      <p class="article-grid-excerpt">
+                        <?php echo mb_strimwidth(get_the_excerpt(), 0, 70, '...');
+                      ?>
+                      </p>
+                      <span class="article-date"><?php the_time( 'j F Y' )?></span>
+                    </a>
+                  </li>
+                <?php 
+                break;
+            }
+            ?>
+            <!-- Вывода постов, функции цикла: the_title() и т.д. -->
+            <?php 
+          }
+        } else {
+      // Постов не найдено
+      }
+
+      wp_reset_postdata(); // Сбрасываем $post
+      ?>
+    </ul>
+    <!-- /.article-grid -->
+  
+  <!-- Подключаем сайдбар -->
+  <?php get_sidebar( )?>
+  </div>
+  <!-- /.main-grid -->
 </div>
 <!-- /.container -->
 
--- functions.php
+++ functions.php
@@ -35,6 +35,146 @@ function enqueue_unitheme_style() {
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_unitheme_style' );
 
+/**
+ * Подключение сайдбара
+ */
+function uni_theme_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Сайдбар на главной', 'uni-theme' ),
+			'id'            => 'main-sidebar',
+			'description'   => esc_html__( 'Добавьте виджеты сюда', 'uni-theme' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'uni_theme_widgets_init' );
+
+/**
+ * Добавление нового виджета Downloader_Widget.
+ */
+class Downloader_Widget extends WP_Widget {
+
+	// Регистрация виджета используя основной класс
+	function __construct() {
+		// вызов конструктора выглядит так:
+		// __construct( $id_base, $name, $widget_options = array(), $control_options = array() )
+		parent::__construct(
+			'downloader_widget', // ID виджета, если не указать (оставить ''), то ID будет равен названию класса в нижнем регистре: downloader_widget
+			'Полезные файлы',
+			array( 'description' => 'Файлы для скачивания', 'classname' => 'widget_downloader', )
+		);
+
+		// скрипты/стили виджета, только если он активен
+		if ( is_active_widget( false, false, $this->id_base ) || is_customize_preview() ) {
+			add_action('wp_enqueue_scripts', array( $this, 'add_downloader_widget_scripts' ));
+			add_action('wp_head', array( $this, 'add_downloader_widget_style' ) );
+		}
+	}
+
+	/**
+	 * Вывод виджета во Фронт-энде
+	 *
+	 * @param array $args     аргументы виджета.
+	 * @param array $instance сохраненные данные из настроек
+	 */
+	function widget( $args, $instance ) {
+    $title = $instance['title'] ;
+    $description = $instance['description'] ;
+    $link = $instance['link'] ;
+
+		echo $args['before_widget'];
+		if ( ! empty( $title ) ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
+    if ( ! empty( $description ) ) {
+			echo '<p>' . $description . '</p>';
+    }
+    if ( ! empty( $link ) ) {
+      echo '<a target="_blank" class="widget-link" href="' . $link . '"><img class="widget-link-icon" src="' . get_template_directory_uri(). '/assets/images/download.svg" >Скачать</a>';
+		}
+    echo $args['after_widget'];
+	}
+
+	/**
+	 * Админ-часть виджета
+	 *
+	 * @param array $instance сохраненные данные из настроек
+	 */
+	function form( $instance ) {
+    $title = @ $instance['title'] ?: 'Полезные файлы';
+    $description = @ $instance['description'] ?: 'Описание ';
+    $link = @ $instance['link'] ?: 'http://yandex.ru ';
+
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Заголовок:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+    <p>
+			<label for="<?php echo $this->get_field_id( 'description' ); ?>"><?php _e( 'Описание:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'description' ); ?>" name="<?php echo $this->get_field_name( 'description' ); ?>" type="text" value="<?php echo esc_attr( $description ); ?>">
+		</p>
+    <p>
+			<label for="<?php echo $this->get_field_id( 'link' ); ?>"><?php _e( 'Ссылка на файл:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'link' ); ?>" name="<?php echo $this->get_field_name( 'link' ); ?>" type="text" value="<?php echo esc_attr( $link ); ?>">
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Сохранение настроек виджета. Здесь данные должны быть очищены и возвращены для сохранения их в базу данных.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance новые настройки
+	 * @param array $old_instance предыдущие настройки
+	 *
+	 * @return array данные которые будут сохранены
+	 */
+	function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+    $instance['description'] = ( ! empty( $new_instance['description'] ) ) ? strip_tags( $new_instance['description'] ) : '';
+    $instance['link'] = ( ! empty( $new_instance['link'] ) ) ? strip_tags( $new_instance['link'] ) : '';
+		return $instance;
+	}
+
+	// скрипт виджета
+	function add_downloader_widget_scripts() {
+		// фильтр чтобы можно было отключить скрипты
+		if( ! apply_filters( 'show_my_widget_script', true, $this->id_base ) )
+			return;
+
+		$theme_url = get_stylesheet_directory_uri();
+
+		wp_enqueue_script('my_widget_script', $theme_url .'/my_widget_script.js' );
+	}
+
+	// стили виджета
+	function add_downloader_widget_style() {
+		// фильтр чтобы можно было отключить стили
+		if( ! apply_filters( 'show_my_widget_style', true, $this->id_base ) )
+			return;
+		?>
+		<style type="text/css">
+			.my_widget a{ display:inline; }
+		</style>
+		<?php
+	}
+
+} 
+// конец класса Downloader_Widget
+
+// регистрация Downloader_Widget в WordPress
+function register_downloader_widget() {
+	register_widget( 'Downloader_Widget' );
+}
+add_action( 'widgets_init', 'register_downloader_widget' );
+
 ## отключаем создание миниатюр файлов для указанных размеров
 add_filter( 'intermediate_image_sizes', 'delete_intermediate_image_sizes' );
 function delete_intermediate_image_sizes( $sizes ){
