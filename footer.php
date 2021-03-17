ffdf2ab830b61ba769df9d53b6676d317c19ac4a
red5tart <red5tart@mail.ru>
2021-03-17 19:08:45 +0300

    12-1 add coloured footer

--- footer.php
+++ footer.php
@@ -1,3 +1,17 @@
+    <footer class="footer">
+      <div class="container">
+        <?php 
+          if ( ! is_active_sidebar( 'sidebar-footer' ) ) {
+            return;
+          }
+          ?>
+          <div class="footer-menu-bar">
+            <?php dynamic_sidebar( 'sidebar-footer' ); ?>
+          </div>
+          <!-- /.footer-menu-bar -->
+      </div>
+      <!-- /.container -->
+    </footer>
   <?php wp_footer();?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -49,6 +49,15 @@ function uni_theme_widgets_init() {
 			'before_title'  => '<h2 class="widget-title">',
 			'after_title'   => '</h2>',
 		));
+				register_sidebar(array(
+			'name'          => esc_html__( 'Меню в подвале', 'uni-theme' ),
+			'id'            => 'sidebar-footer',
+			'description'   => esc_html__( 'Добавте сюда меню', 'uni-theme' ),
+			'before_widget' => '<section id="%1$s" class="footer-menu %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="footer-menu-title">',
+			'after_title'   => '</h2>',
+		));
 }
 add_action( 'widgets_init', 'uni_theme_widgets_init' );
 
