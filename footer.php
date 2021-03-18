3c221bfe7a83e39fa074f8db7dc246ded1f20fd4
red5tart <red5tart@mail.ru>
2021-03-18 20:33:01 +0300

    12-4  front page completed

--- footer.php
+++ footer.php
@@ -1,38 +1,44 @@
     <footer class="footer">
       <div class="container">
-        <?php 
-          if ( ! is_active_sidebar( 'sidebar-footer' ) ) {
-            return;
-          }
-          ?>
-          <div class="footer-menu-bar">
-            <?php dynamic_sidebar( 'sidebar-footer' ); ?>
-          </div>
-          <!-- /.footer-menu-bar -->
-          <div class="footer-info">
-            <?php 
-              wp_nav_menu( [
-                'theme_location'  => 'footer_menu',
-                'container'       => 'nav', 
-                'menu_class'      => 'footer-nav', 
-                'echo'            => true,
-              ] );        
+        <div class="footer-menu-bar">
+          <?php dynamic_sidebar( 'sidebar-footer' ); ?>
+        </div>
+        <!-- /.footer-menu-bar -->
+        <div class="footer-info">
+          <?php 
+            wp_nav_menu( [
+              'theme_location'  => 'footer_menu',
+              'container'       => 'nav', 
+              'menu_class'      => 'footer-nav', 
+              'echo'            => true,
+            ] );        
 
-              $instance = array (
-                'title' => '',
-                'link1' => 'http:/facebook.com',
-                'link2' => 'http:/instagram.com',
-                'link3' => 'http:/vkontakte.com',
-                'link4' => 'http:/twitter.com'                         
-              );
-              $args = array (
-                'before_widget' => '<div class="footer-social">',
-                'after_widget' => '</div>',
-              );
-              the_widget( 'Social_Widget', $instance, $args );
-            ?>
-          </div>
-          <!-- /.footer-info -->
+            $instance = array (
+              'title' => '',
+              'link1' => 'http:/facebook.com',
+              'link2' => 'http:/instagram.com',
+              'link3' => 'http:/vkontakte.com',
+              'link4' => 'http:/twitter.com'                         
+            );
+            $args = array (
+              'before_widget' => '<div class="footer-social">',
+              'after_widget' => '</div>',
+            );
+            the_widget( 'Social_Widget', $instance, $args );
+          ?>
+        </div>
+        <!-- /.footer-info -->
+        <?php 
+        if ( ! is_active_sidebar( 'sidebar-footer' ) ) {
+          return;
+        }
+        ?>
+        <div class="footer-text-wrapper">
+          <?php dynamic_sidebar( 'sidebar-footer-text' ); ?>
+          <span class="footer-copyright"><?php echo date('Y') . ' &copy; ' . get_bloginfo( 'name' );?></span>
+          <!-- /.footer-copyright -->
+        </div>
+        <!-- /.footer-text-wrapper -->
       </div>
       <!-- /.container -->
     </footer>
--- functions.php
+++ functions.php
@@ -52,11 +52,20 @@ function uni_theme_widgets_init() {
 				register_sidebar(array(
 			'name'          => esc_html__( 'Меню в подвале', 'uni-theme' ),
 			'id'            => 'sidebar-footer',
-			'description'   => esc_html__( 'Добавте сюда меню', 'uni-theme' ),
+			'description'   => esc_html__( 'Добавьте сюда меню', 'uni-theme' ),
 			'before_widget' => '<section id="%1$s" class="footer-menu %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="footer-menu-title">',
 			'after_title'   => '</h2>',
+		));
+				register_sidebar(array(
+			'name'          => esc_html__( 'Текст в подвале', 'uni-theme' ),
+			'id'            => 'sidebar-footer-text',
+			'description'   => esc_html__( 'Добавьте сюда текст', 'uni-theme' ),
+			'before_widget' => '<section id="%1$s" class="footer-text %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<>',
+			'after_title'   => '<>',
 		));
 }
 add_action( 'widgets_init', 'uni_theme_widgets_init' );
