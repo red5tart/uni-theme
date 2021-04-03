60ccfde8e2c9705e1f908fd1d4988570c384433e
red5tart <red5tart@mail.ru>
2021-04-03 22:27:29 +0300

    16-3 post sidebar styles

--- sidebar-post.php
+++ sidebar-post.php
@@ -8,7 +8,7 @@
 ?>
 
 <div class="sidebar-post-wrapper">
-  <ul>
+  <ul class="sidebar-post">
     <?php
     
     $category = get_the_category();
@@ -16,9 +16,9 @@
     $args = array( 'numberposts' => 4, 'orderby' => 'rand', 'cat' =>$cat_add_id, 'post__not_in'  => array(get_the_ID()));
     $rand_posts = get_posts( $args );
     foreach( $rand_posts as $post ) : ?>
-      <li>
-        <a href="<?php the_permalink(); ?>">
-          <img width="263" height="180" src="<?php 
+      <li class="sidebar-post-item">
+        <a href="<?php the_permalink(); ?>" class="sidebar-post-item-permalink">
+          <img src="<?php 
           if( has_post_thumbnail() ) {
               echo get_the_post_thumbnail_url( null, 'hotnews-thumb');
             }
@@ -26,9 +26,10 @@
               echo get_template_directory_uri().'/assets/images/img-default.png';
             }
           ?>" alt="">
-          <?php the_title(); ?>
-            <div class="views">
-              <svg width="15" height="11" class="icon comments-icon-white">
+          <h4 class="sidebar-post-title"><?php the_title(); ?></h4>
+          <div class="sidebar-post-footer">
+            <div class="comments">
+              <svg width="15" height="11" class="icon comments-icon">
                 <use xlink:href="<?php echo get_template_directory_uri()?>/assets/images/sprite.svg#eye"></use>
               </svg>
               <span class="comments-counter">
@@ -36,16 +37,19 @@
               </span>
             </div><!-- /.views -->
             <div class="comments">
-              <svg width="15" height="15" class="icon comments-icon-white">
+              <svg width="15" height="15" class="icon comments-icon">
                 <use xlink:href="<?php echo get_template_directory_uri()?>/assets/images/sprite.svg#comment"></use>
               </svg>
               <span class="comments-counter">
                 <?php comments_number('0', '1', '%') ?>
               </span>
             </div> <!-- /.comments -->
+          </div>
+          <!-- /.sidebar-post-footer -->
+
         </a>
       </li>
     <?php endforeach; ?>
     <?php wp_reset_postdata() ?>
-  </ul>`
+  </ul>
 </div>
